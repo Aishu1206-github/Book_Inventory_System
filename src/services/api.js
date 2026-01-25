377a67d6ab29fc5e7619cc23e15079184e4b37bc
Aishwarya Jadhav <aj5545007@gmail.com>
2026-01-25 10:19:36 +0530

    uploaded dj.json and api.js file

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,15 @@
 
+import axios from "axios";
+
+const URL = "http://localhost:5000/books";
+// Read - All books
+export const getBooks = () => axios.get(URL);
+// Reas - single book by id
+export const getBook = (id) => axios.get(`${URL}/${id}`);
+// create - add new book
+export const addBook = (book) => axios.post(URL, book);
+// update - update book by id
+export const updateBook = (id, book) => axios.put(`${URL}/${id}`, book);
+// delete - delete book by id
+export const deleteBook =(id) => axios.delete(`${URL}/${id}`);
+
