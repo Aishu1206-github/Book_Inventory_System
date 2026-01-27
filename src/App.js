8300f971b68790dc9a5b77ed5ecf96ebae72ec30
Aishwarya Jadhav <aj5545007@gmail.com>
2026-01-27 20:33:57 +0530

    changes on App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BookList from './components/BookList';
 import BookDetails from './components/BookDetails';
